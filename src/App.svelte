5a2d90844e907850d04c4f5a2931b9131a8e87c3
uniquemo <myx3901051@qq.com>
2021-06-29 17:24:23 +0800

    refactor: directory structure

--- src/App.svelte
+++ src/App.svelte
@@ -1,67 +1,13 @@
 <script lang="ts">
-  import foodData from './data/food'
-  import type { IFood } from './types/food'
-
-  let running: boolean = false;
-  let food: IFood | null = null;
-  let foodName: string = '';
-  let timer: NodeJS.Timeout | null = null;
-
-  const getRandomIndex = (min: number, max: number): number => {
-    return Math.min(Math.floor(Math.random() * (max - min) + min), max);
-  }
-
-  const start = () => {
-    running = true;
-
-    timer = setInterval(() => {
-      const idx = getRandomIndex(0, foodData.length);
-      food = foodData[idx];
-      foodName = food.name;
-    }, 50)
-  }
-
-  const stop = () => {
-    running = false;
-    if (timer) {
-      clearInterval(timer);
-    }
-  }
+  import Food from './components/food/index.svelte'
 </script>
 
 <main>
-  <h3>Food Selection</h3>
-  <input
-    disabled
-    placeholder="Generate food randomly"
-    class="food"
-    bind:value={foodName}
-  />
-
-  {#if !running}
-    <button on:click={start}>start</button>
-  {:else}
-    <button on:click={stop}>stop</button>
-  {/if}
-
-  {#if food && !running}
-    <div>
-      <h4>Decision Making Result</h4>
-      {#each Object.keys(food) as key}
-        <div>{key}: {food[key]}</div>
-      {/each}
-    </div>
-  {/if}
+  <Food />
 </main>
 
 <style>
   main {
     padding: 0 1em;
-  }
-  input:disabled {
-    cursor: not-allowed;
-  }
-  .food {
-    width: 300px;
   }
 </style>
